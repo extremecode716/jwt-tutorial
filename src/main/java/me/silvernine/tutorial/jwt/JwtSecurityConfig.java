bfd3658ab0b0cefba9a74cfa95c56f66045f270f
ExtremeCode <extremecode716@gmail.com>
2021-07-16 03:19:54 +0900

    [Add] Security 로직에 필터 등록

---
+++ src/main/java/me/silvernine/tutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,24 @@
+package me.silvernine.tutorial.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * TokenProvider, JwtFilter 를 SecurityConfig 에 적용할때 사용할 JwtSecurityConfig 클래스
+ * SecurityConfigurerAdapter 를 extends 하고 TokenProvider 를 주입받아서 configure 메소드 안에서 JwtFilter 를 통해 Security 로직에 필터를 등록합니다.
+ */
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
